ec4e20af386906edced4f9fb7550106fd6b0c4f6
LORRANDO <lorrando.02@gmail.com>
2026-06-08 21:06:43 +0300

    Commit
    portal_frame_custom
    remuve_waystoneswarp_plate

---
+++ server_scripts/portal_frame_custom.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(event => {
+  event.remove({ output: 'irons_spellbooks:portal_frame' })
+
+  event.shaped('irons_spellbooks:portal_frame', [
+    'MEM',
+    'AOA',
+    'AHA'
+  ], {
+    M: 'irons_spellbooks:mithril_ingot',
+    E: 'minecraft:ender_pearl',
+    A: 'irons_spellbooks:arcane_ingot',
+    O: 'distant_worlds:salmoran_eye',
+    H: 'distant_worlds:helyst_crystal'
+  })
+})
